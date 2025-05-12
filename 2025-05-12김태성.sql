393358e67fa07f3d988c4d67d471b9654e699356
fulldog15 <lovelove080109@gmail.com>
2025-05-12 11:22:15 +0900

    Add files via upload

---
+++ 2025-05-12김태성.sql
@@ -0,0 +1,11 @@
+select 3+3 from dual;
+select '2'+'3' from dual;
+select 'a'+'b' from dual;
+select to_number('5') from dual;
+select to_number('A') from dual;
+select ascii('A') from dual;
+select to_date('24/4/28')+1 from dual;
+select ename,comm,NVL(comm,0),NVL(comm,100) from emp where deptno = 30;
+select empno,ename,sal,comm,NVL2(comm,sal+comm,sal*0)"NVL2" from emp where deptno = 30;
+select deptno,name,DECODE(deptno,101,'Comp-uter Engineering')"DNAME" from professor;
+select empno,ename,sal,case when sal between 1 and 1000 then 'level1' when sal between 1001 and 2000 then 'level2' when sal between 2001 and 3000 then 'level3' when sal between 2001 and 3000 then 'level4' else 'level5' end"LEVEL" from emp;
